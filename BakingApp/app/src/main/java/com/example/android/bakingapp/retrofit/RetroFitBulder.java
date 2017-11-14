e5f6941283de84b9e085f7c3486de92cc9a139fc
mateus.macedo <mhbassomacedo@gmail.com>
2017-11-14 15:51:16 -0200

    Add retrofit builder
    Signed-off-by: mateus.macedo <mhbassomacedo@gmail.com>

--- BakingApp/app/src/main/java/com/example/android/bakingapp/retrofit/RetroFitBulder.java
+++ BakingApp/app/src/main/java/com/example/android/bakingapp/retrofit/RetroFitBulder.java
@@ -1,8 +1,34 @@
 package com.example.android.bakingapp.retrofit;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by lsitec101.macedo on 14/11/17.
  */
 
 public class RetroFitBulder {
+
+    public static RecipeRetroFit recipeRetroFit;
+
+    public static RecipeRetroFit Retrieve() {
+
+        Gson gson = new GsonBuilder().create();
+
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+
+
+        recipeRetroFit = new Retrofit.Builder()
+                .baseUrl("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .callFactory(httpClientBuilder.build())
+                .build().create(RecipeRetroFit.class);
+
+
+        return recipeRetroFit;
+    }
 }
