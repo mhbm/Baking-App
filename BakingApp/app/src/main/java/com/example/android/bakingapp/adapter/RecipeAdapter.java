6295fb6e900faaec46138d22e419bc9da207e20e
mateus.macedo <mhbassomacedo@gmail.com>
2017-11-14 16:08:27 -0200

    Add Picasso and do some modifications
    Signed-off-by: mateus.macedo <mhbassomacedo@gmail.com>

--- BakingApp/app/src/main/java/com/example/android/bakingapp/adapter/RecipeAdapter.java
+++ BakingApp/app/src/main/java/com/example/android/bakingapp/adapter/RecipeAdapter.java
@@ -1,8 +1,95 @@
 package com.example.android.bakingapp.adapter;
 
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android.bakingapp.R;
+import com.example.android.bakingapp.data.RecipeModel;
+
+import java.util.ArrayList;
+
 /**
  * Created by lsitec101.macedo on 14/11/17.
  */
 
-public class RecipeAdapter {
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecyclerViewHolder> {
+
+    private final ListItemClickListener mOnLickListener;
+    ArrayList<RecipeModel> mRecipes;
+    Context mContext;
+
+    public RecipeAdapter(ListItemClickListener listener) {
+        mOnLickListener = listener;
+    }
+
+    public interface ListItemClickListener {
+        void onListItemClick(RecipeModel clickedItemIndex);
+    }
+
+    public void setRecipeData(RecipeModel recipeModel, Context context) {
+        mRecipes.add(recipeModel);
+        mContext = context;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecipeAdapter.RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.grid_item_layout;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(layoutIdForListItem, parent,  false);
+        RecyclerViewHolder viewHolder = new RecyclerViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        holder.textRecyclerView.setText(mRecipes.get(position).getName());
+        String imageUrl=mRecipes.get(position).getImage();
+
+        if (imageUrl!="") {
+            Uri builtUri = Uri.parse(imageUrl).buildUpon().build();
+            Picasso.with(mContext).load(builtUri).into(holder.imageRecyclerView);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mRecipes.isEmpty()) {
+            return 0;
+        } else {
+            return mRecipes.size();
+        }
+    }
+
+    class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView textRecyclerView;
+        ImageView imageRecyclerView;
+
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+
+            textRecyclerView = itemView.findViewById(R.id.title_recipe);
+            imageRecyclerView = itemView.findViewById(R.id.recipe_image);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mOnLickListener.onListItemClick(mRecipes.get(clickedPosition));
+        }
+
+    }
 }
